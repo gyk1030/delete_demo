7400f2b782320832f2e0a770ad7c1dfbf99d3b93
gaoyongkang <13572551532@163.com>
2020-04-03 17:48:57 +0800

    a threed version

--- app01/views.py
+++ app01/views.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 from rest_framework.views import APIView
+from django.http import JsonResponse
+from django.contrib import auth
 
 class Test(APIView):
-    def get(self,request, pk):
+    def get(self, request, pk):
         print pk
         service_id = request.GET.get('service_id')
         return HttpResponse(service_id)
 
-    def delete(self, request ,pk):
+    def post(self, request):
+        # user = request.data.get('name')
+        # pwd = request.data.get('pwd')
+        print request.session.__dict__
+        # if user and pwd:
+        #     print user + '+' + pwd
+        return JsonResponse(str(request.session.__dict__))
+
+    def delete(self, request, pk):
         print pk
         service_id = request.GET.get('service_id')
         return HttpResponse(service_id)
--- delete_demo/urls.py
+++ delete_demo/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from app01 import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^demo/$', views.Test.as_view()),
     url(r'^demo/(?P<pk>\d+)$', views.Test.as_view()),
 
 ]
